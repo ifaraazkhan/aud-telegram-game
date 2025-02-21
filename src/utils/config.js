4f0dcff3a5421848dfa3fb57d1d3d5a41aff653d
codewithfkhan <fkhan@techbrainscorp.com>
2025-02-21 20:37:44 +0530

    ss

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,6 +1,6 @@
-const access_token = `eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI1ODQ2MjBiZS05ZjU0LTQxYzYtYjZiMy1hZjI5YjBhOTNjZmMiLCJhdXRob3JpdHkiOiJ1c2VyIiwia2VlcF9sb2dnZWRfaW4iOiJZIiwiaWF0IjoxNzM5NDM1MzExLCJleHAiOjE3NzA5NzEzMTF9.fmbY0f2y9ra4S5FEI8JvBkXw0YqL0eFieZkKCHJJIA2Kax053w1FQLj5bibayxL1MWqTJnKovFMyOgVUQL6-Zg`
-const API_URL = 'http://localhost:4200'
-// const API_URL = 'https://cms.datplatform.com'
+const access_token = `eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI0YmExY2Y2My1iMzA3LTRlNTAtYTY2OS02ZDliZjRjZjc1NmMiLCJhdXRob3JpdHkiOiJ1c2VyIiwia2VlcF9sb2dnZWRfaW4iOiJZIiwiaWF0IjoxNzQwMTM5NjE4LCJleHAiOjE3NzE2NzU2MTh9.dZ5dJLFCHhD1yiWUIUShiB7m3xG8YrIvdiQwzwjq9QQBPbF6VbJnBH7b4KucL6fNI99KTP-TsHwU3lHpAwespg`
+// const API_URL = 'http://localhost:4200'
+ const API_URL = 'https://cms.datplatform.com'
 export const CustomConfig = {
     access_token,
     API_URL
