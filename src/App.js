690292fbe9d7b2fc55c57440c1094c2a63e47428
avneeshcse <avneeshcse@gmail.com>
2025-02-21 20:24:07 +0530

    fixing minor issue

--- src/App.js
+++ src/App.js
@@ -198,8 +198,9 @@ function App() {
   const getUserRewardsBalance = async () => {
     // Fetch user rewards
     const sdk = K2SDK.getInstance();
-    const rDetails = await sdk.getRewardBalance();
-    if (rDetails) {
+    const rewardRes = await sdk.getRewardBalance(publisherId, { user_unique_id: 12345678 });
+    if (rewardRes && rewardRes.status == 200) {
+      const rDetails = rewardRes.results
       setRewardDetails(rDetails)
       setRewardsBal(`${rDetails.reward_balance} ${rDetails?.reward_currency ?? ''}`)
     }
